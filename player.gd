82c6cf9d0cf4009b2b7f26aef3988131ba9afae2
Jover Lee <joverlee521@gmail.com>
2024-04-06 16:50:04 -0700

    Script the player movements
    Move the Player in four directions with keyboard inputs

---
+++ player.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+# Using the export keyword allows variable to be edit in the Inspector
+# Changing value in the Inspector will override default value set here
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	# Inputs had to be set up in Project -> Project Settings -> Input Map
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		# $ is shorthand for get_node(), so this is same as get_node("AnimatedSprite2D").play()
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
