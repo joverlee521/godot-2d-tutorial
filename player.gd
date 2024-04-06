b4f1b679f4e89ac662a898f251a9ce1699c5f72e
Jover Lee <joverlee521@gmail.com>
2024-04-06 16:56:26 -0700

    Link player animations
    Link player movement with previously set up animations.

--- player.gd
+++ player.gd
@@ -32,3 +32,11 @@ func _process(delta):
 
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
